3c4197e574fce9cdb9ab8062facbc89d9ecbd1b8
Joe Mafrici <hi@gojoe.dev>
2024-12-14 14:12:23 -0800

    get size of contents in directory

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,40 @@
 use std::fs;
+use walkdir::WalkDir;
+
+const BYTES_PER_GB: f64 = 1024.0 * 1024.0 * 1024.0;
+fn bytes_to_gb(bytes: u64) -> f64 {
+    bytes as f64 / BYTES_PER_GB
+}
 
 fn main() {
     println!("disk-analyzer starting...");
-    let read_dir_result= fs::read_dir("/Users/deepwater/code");
-    let read_dir = match read_dir_result {
-        Ok(read_dir) => read_dir,
-        Err(error) => panic!("Error while reading directory: {:?}", error),
-    };
+    let root_dir = "/Users/deepwater/code";
+    let mut size: u64 = 0;
+    let mut num_files = 0;
 
-    for dir_entry_result in read_dir {
-        let dir_entry = match dir_entry_result {
-            Ok(dir_entry) => dir_entry,
-            Err(error) => panic!("Error while reading directory entry: {:?}", error),
+    for entry in WalkDir::new(root_dir) {
+        let entry = match entry {
+            Ok(entry) => entry,
+            Err(error) => {
+                println!("Error reading directory: {:?}", error);
+                continue
+            },
         };
-        println!("{:?}", dir_entry.file_name());
+        if entry.file_type().is_file() {
+            match entry.metadata() {
+                Ok(metadata) => {
+                    size += metadata.len();
+                    num_files += 1;
+                },
+                Err(error) => {
+                    println!("Error reading metadata: {:?}", error);
+                    continue
+                }
+            }
+        }
     }
+
+    println!("directory: {} size (GB): {} number of files: {}", root_dir, size, num_files);
 
     println!("disk-analyzer stopping...");
 }
